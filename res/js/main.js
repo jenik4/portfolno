3744dbacedb0781c82daea60a77e6dde216dd6a8
Jan Filipec <115136206+jenik4@users.noreply.github.com>
2023-03-08 08:13:44 +0100

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,14 @@
+const prvni = document.getElementById("prvni");
+const druhy = document.getElementById("druhy");
+const treti = document.getElementById("treti");
+
+prvni.onmouseout = () => {
+
+    prvni.style.transitionDuration = "1s";
+}
+druhy.onmouseout = () => {
+    druhy.style.transitionDuration = "1s";
+}
+treti.onmouseout = () => {
+   treti.style.transitionDuration = "1s";
+}
